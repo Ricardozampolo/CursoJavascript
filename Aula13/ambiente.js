70e1334a04f0c95b41cd1e9f92769f907aa01328
Ricardo Zampolo <kakazampolo2002@gmail.com>
2023-03-07 10:05:49 -0300

    class 13

---
+++ Aula13/ambiente.js
@@ -0,0 +1,25 @@
+//Loop com while com condição no inicio
+
+var inicio = 1;
+
+while (inicio <= 600) {
+    console.log(`Passo ${inicio}`);
+    inicio++;
+}
+
+//Loop com while com condição no final
+
+var fim = 1;
+
+do{
+    console.log(`Passo ${fim}`);
+    fim++;
+} while(fim <= 600)
+
+
+//Loop com for 
+var p;
+
+for(p = 1; p <= 600; p++){
+    console.log(`Passo ${p}`);
+}
